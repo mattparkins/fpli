d7f3be7db51cad66090626851e5cdd720dff40cc
Matt Parkins <matt@mooflower.com>
2022-08-15 13:21:50 +0100

    added historic data, tidied up

---
+++ src/Analysis/AnalyserFixture.cs
@@ -0,0 +1,105 @@
+using System.Runtime.Serialization;
+namespace fpli {
+	public class FixtureAnalyser : Analyser {
+		
+		public FixtureAnalyser(FPLData fpl, Config config): base(fpl, config) {
+			if (_config.gameweek <= 0) {
+				Program.Quit("Gameweek is invalid or not set");
+			}
+			
+			if (_config.fixtureCount <= 0) {
+				Program.Quit("FixtureCount is invalid or not set");
+			}
+
+			if (_config.fixturePicks.Count >= _config.fixtureCount) {
+				Program.Quit("FixturesAlreadyPicked count >= Gameweeks to analyse");
+			}
+		}
+
+		public override async Task PreFetch() {
+			if (_config.gameweek + _config.fixtureCount -1 >  _fpl.Bootstrap.events.Count) {
+				Program.Quit("Gameweek and Analysis window extend beyond end of the season");
+			}
+
+			int i = _config.fixtureCount;
+			while (i-- > 0) {
+				await _fpl.LoadFixtures(_config.gameweek + i);
+			}
+		}
+
+
+		// Check and fix any data invalidity
+		public override void Preprocess() {}
+
+		enum Venue {
+			HOME = 0,
+			AWAY = 1,
+		}
+
+		int[] _minStrength = new int[2];
+		int[] _maxStrength = new int[2];
+
+		private void _calculateTeamStrengths() {
+			// Use FPL home/away strengths for now, but find the max/med/min
+			_minStrength[(int) Venue.HOME] = (int) _fpl.Bootstrap.teams.Min(t => t.strength_overall_home);
+			_maxStrength[(int) Venue.HOME] = (int) _fpl.Bootstrap.teams.Max(t => t.strength_overall_home);
+			_minStrength[(int) Venue.AWAY] = (int) _fpl.Bootstrap.teams.Min(t => t.strength_overall_away);
+			_maxStrength[(int) Venue.AWAY] = (int) _fpl.Bootstrap.teams.Max(t => t.strength_overall_away);
+
+			Console.WriteLine($"Home min/med/max strengths: {_minStrength[(int) Venue.HOME]} {_maxStrength[(int) Venue.HOME]}");
+			Console.WriteLine($"Away min/med/max strengths: {_minStrength[(int) Venue.AWAY]} {_maxStrength[(int) Venue.AWAY]}");
+
+			// Todo: foreach team, as home work out the WE against each possible opponent.
+			// Use double as the numbers will get small after 8 depth
+			// Win expectancy is a good start, but WE includes draw points - is there a way of removing them using historic data?
+			// Perhaps reducing win % by the proportion of points that come from draws?  (would have to consider win to be worth 2pts, draw 1pt)
+
+			// Go through every fixture this week
+			_fpl.Fixtures[2].ForEach(f => {
+				int homeId = f.team_h;
+				int awayId = f.team_a;
+				float weHome = _winExpectancyHome(homeId, awayId);
+				float weAway = _winExpectancyAway(homeId, awayId);
+				string homeName = _fpl.Bootstrap.teams.Find(t => t.id == homeId).short_name;
+				string awayName = _fpl.Bootstrap.teams.Find(t => t.id == awayId).short_name;
+				string homeGoals = f.team_h_score?.ToString() ?? "TBD";
+				string awayGoals = f.team_a_score?.ToString() ?? "TBD";
+				Console.WriteLine($"WE for {homeName} ({weHome:0.00}) vs ({weAway:0.00}) {awayName}, actual {homeGoals}-{awayGoals} ");
+			});
+		}
+
+		public override void Analyse() {
+			_calculateTeamStrengths();
+
+			// For each searchdepth from forcedMoveCount+1 to targetDepth
+			// 		Initialise engine
+			// 		Play forced moves
+			// 		Enumerate "root" moves
+			// 		Foreach root move recurse
+			//			Make move
+			// 				If not at leaf 
+			//					enumerate moves
+			//					foreach move recurse
+			//				else 
+			//					evaluate
+			//			undo move
+			//		return best evaluation
+		}
+
+		float _winExpectancyHome(int homeId, int awayId) {
+			Team home = _fpl.Bootstrap.teams.Find(t => t.id == homeId);
+			Team away = _fpl.Bootstrap.teams.Find(t => t.id == awayId);
+
+			float dr = (away.strength_overall_away - home.strength_overall_home);
+			return (float) (1f/(Math.Pow(10f, dr/400f) +1f));
+		}
+
+		float _winExpectancyAway(int homeId, int awayId) {
+			Team home = _fpl.Bootstrap.teams.Find(t => t.id == homeId);
+			Team away = _fpl.Bootstrap.teams.Find(t => t.id == awayId);
+
+			float dr = (home.strength_overall_home - away.strength_overall_away);
+			return (float) (1f/(Math.Pow(10f, dr/400f) +1f));
+		}
+	}
+}
---
+++ src/Analysis/AnalyserMiniLeagues.cs
@@ -0,0 +1,152 @@
+namespace fpli {
+	public class MiniLeagueAnalyser : Analyser {
+
+		public MiniLeagueAnalyser(FPLData fpl, Config config): base(fpl, config) {
+			// Basic config sanity check
+			if (_config.leagueId <= 0) {
+				Program.Quit("LeagueId is invalid or not set");
+			}
+
+			if (_config.maxManagers <= 0) {
+				Program.Quit("MaxManagers is invalid");
+			}
+		}
+
+		public override async Task PreFetch() {
+			
+			// Load the league
+			await _fpl.LoadLeague(_config.leagueId, _config.maxManagers);
+
+			// Load each manager in the league
+			foreach(var standingEntry in _fpl.Standings[_config.leagueId].standings.results) {
+				await _fpl.LoadManager(standingEntry.entry);
+			};
+		}
+
+
+		// Check and fix any data invalidity
+		public override void Preprocess() {
+
+			// Pre-GW1, not sure what to do, not mini-league analysis thats's for sure.
+			if (_fpl.Bootstrap.GetCurrentGameweekId() <= 0) {
+				Program.Quit("Cannot execute Mini-League Analysis if GW is <= 0");
+			}
+		}
+
+
+		public override void Analyse() {
+			_reportChipUsage();
+			_reportCaptaincy();
+			_reportHits();
+
+			// Points on bench ?
+			// Transfer evaluation - should this be a post-week run ?
+		}
+
+
+		void _reportChipUsage() {
+			
+			int totalChipsPlayed = 0;
+			var list = _fpl.Standings[_config.leagueId].ChipUsage.OrderByDescending(v => v.Value.Count);
+			
+			foreach(var kv in list) {
+				if (kv.Key != "none") { 
+					totalChipsPlayed += kv.Value.Count;
+				}
+			}
+
+			if (totalChipsPlayed == 0) {
+				Console.WriteLine("\nNo Active Chips\n");
+				return;
+			}
+
+			Console.WriteLine("\nActive Chips\n");
+
+			foreach(var kv in list) {
+				if (kv.Key != "none") {
+					Console.Write($"{kv.Value.Count, 4} {kv.Key}: (");
+					string glue = "";
+					kv.Value.ForEach(entryId => {
+
+						// Get the name of the manager, we could load this from a manager info file
+						// but it's one less fetch per entry to just take it from the leaderboard
+
+						Manager manager = _fpl.Managers[entryId];
+						Result result = _fpl.Standings[_config.leagueId].standings.results.Find(r => r.entry == entryId);
+						Console.Write($"{glue}{result.player_name}");
+						glue = ", ";
+					});
+					Console.WriteLine(")");
+				}
+			}
+		}
+
+
+		void _reportCaptaincy() {
+			Console.WriteLine("\nCaptaincy\n");
+
+			var list = _fpl.Standings[_config.leagueId].Captaincy.OrderByDescending(v => v.Value.Count);
+			int totalManagers = _fpl.Managers.Count;	// this seems risky - what if we've loaded another league??  Test this league!
+
+			foreach(var kv in list) {
+
+				_sb.Length = 0;
+				Element captain = _fpl.Bootstrap.GetElement(kv.Key);
+				_sb.Append($"{kv.Value.Count, 4} {captain.web_name}");
+
+				if (_config.incManagersInCaptaincy) {
+					string glue = ": ";
+
+					kv.Value.ForEach(entryId => {
+						Manager manager = _fpl.Managers[entryId];
+						Result result = _fpl.Standings[_config.leagueId].standings.results.Find(r => r.entry == entryId);
+						_sb.Append($"{glue}{result.player_name}");
+						glue = ", ";
+					});
+					
+					_sb.Append(".");
+				}
+
+				Console.WriteLine(_sb.ToString());
+			}
+		}	
+
+		void _reportHits() {
+
+			var list = _fpl.Managers.OrderByDescending(m => m.Value.GetTransferCost);
+
+			int totalHitPoints = 0;
+			int largestHitPoints = list.First().Value.GetTransferCost;
+			int largestHitCount = 0;
+			int noHitCount = 0;
+			int rollCount = 0;
+
+			foreach (var kv in list) {
+				totalHitPoints += kv.Value.GetTransferCost;
+				largestHitCount += kv.Value.GetTransferCost == largestHitPoints ? 1 : 0;
+				noHitCount += kv.Value.GetTransferCost == 0 ? 1 : 0;
+				rollCount += kv.Value.DidRoll ? 1 : 0;
+			}
+	
+			Console.WriteLine($"\nAverage hit: {totalHitPoints / (float) list.Count():0.00} pts");	
+
+			if (largestHitPoints != 0) {	
+				Console.Write($"Biggest hit: {largestHitPoints}pts (");
+				
+				string glue = "";
+				foreach (var kv in list) {
+					if (kv.Value.GetTransferCost != largestHitPoints) {
+						break;
+					}
+
+					Result result = _fpl.Standings[_config.leagueId].standings.results.Find(r => r.entry == kv.Value.GetEntryId);
+					Console.Write(glue + result.player_name);
+					glue = ", ";
+				}
+				Console.WriteLine(")");
+			}
+			Console.WriteLine($"No hits:     {noHitCount} players");
+			Console.WriteLine($"Rolled:      {rollCount} players");
+		}
+	}
+}
