22865b7284bb7ecd833f9bbc099370da63d27a0d
Matt Parkins <matt@mooflower.com>
2022-08-16 18:35:43 +0100

    engine first stab

--- src/Analysis/AnalyserFixture.cs
+++ src/Analysis/AnalyserFixture.cs
@@ -42,6 +42,7 @@ namespace fpli {
 		int[] _forcedMoves;
 
 		double[,] we = new double[21,21];	// 0,0 team id's are one-indexed
+		double[,] teamFixtureWE;			// gameweek, teamid
 
 		private void _calculateTeamStrengths() {
 			// Use FPL home/away strengths for now, but find the max/med/min
@@ -70,6 +71,20 @@ namespace fpli {
 				}
 			}
 			Console.WriteLine("");
+
+			// Populate teamFixtureWE
+			teamFixtureWE = new double[_config.fixtureCount +1, 21];
+
+			for (int gw = _config.gameweek; gw < _config.gameweek + _config.fixtureCount; gw++) {
+				int index = _config.gameweek + _config.fixtureCount - gw;
+
+				// For every fixture in this gameweek
+				_fpl.Fixtures[gw].ForEach(fix => {
+					teamFixtureWE[index, fix.team_h] = _winExpectancyHome(fix.team_h, fix.team_a);
+					teamFixtureWE[index, fix.team_a] = _winExpectancyAway(fix.team_h, fix.team_a);
+				});
+			}
+
 
 			// Use double as the numbers will get small after 8 depth
 			// Win expectancy is a good start, but WE includes draw points - is there a way of removing them using historic data?
@@ -103,26 +118,78 @@ namespace fpli {
 			_calculateTeamStrengths();
 			_buildForcedMoves();
 
+			const int resetBoard = (1 << 21) -2; // 0x1FFFFE, 20 bits set, offset by 1
+
 			int forcedMoveCount = _config.fixturePicks.Count;
-			int minimumDepth = forcedMoveCount > 0 ? forcedMoveCount : 1;
-			int targetDepth = minimumDepth;
-			while (targetDepth++ < _config.fixtureCount) {
+			int minimumDepth = forcedMoveCount +1;
+			int targetDepth = minimumDepth -1;
+			while (++targetDepth < _config.fixtureCount) {
 				
 				// Initialise engine
 
-				TeamScoreEval[] eval = new TeamScoreEval[targetDepth];
+				// The board is a bit array where the team id is an index to the bit
+				// which if set has been used at this level or higher
+				int[] board = new int[targetDepth +1];  
+				for (int i = 0; i <= targetDepth; i++) {
+					board[i] = resetBoard;
+				}
+				
+				// The index into each move, which coincidentally is also the teamid
+				int[] moveIndex = new int[targetDepth];
 
+				TeamScoreEval[] eval = new TeamScoreEval[targetDepth];
 				int depth = targetDepth;
 
 				// Play forced moves
 				while (depth > targetDepth - forcedMoveCount) {
+					int teamId = _forcedMoves[targetDepth - depth];
 					depth--;
-					eval[depth].team = _forcedMoves[targetDepth - depth];
+					eval[depth].team = teamId;
 					eval[depth].eval = 1f;
+					board[depth] = board[depth +1] & ~(1 << teamId);
 				} 
 				
-				// 
+				// Iterate through root moves
+				double bestScore = 0;
+				TeamScoreEval[] bestLine = null;
 
+				depth--;
+				int rootDepth = depth;
+				while (moveIndex[rootDepth] <= 20) {
+					
+					while (moveIndex[depth] <= 20) {
+
+						// Go the next move
+						moveIndex[depth]++;
+						board[depth] >>= 1;
+						
+						// Has this team already been used ?
+						if ((board[depth] & 1) == 1) {
+							
+							// No, so add to the eval and make the move 
+							eval[depth].team = moveIndex[depth];	// Add team to the eval
+							eval[depth].eval = eval[depth +1].eval * teamFixtureWE[targetDepth -depth, moveIndex[depth]];	// Multiply the score in
+							board[depth] = board[depth +1] & ~(1 << moveIndex[depth]);							// Mark team as used
+
+							// If we're not at the leaf we need to set up the recurse
+							if (depth > 0) {
+								depth--;
+								moveIndex[depth] =0;
+								board[depth] = board[depth +1];
+							} else {
+								// If we're at the leaf, check if it's a new best score
+								if (eval[depth].eval > bestScore) {
+									bestScore = eval[depth].eval;
+									bestLine = (TeamScoreEval[]) eval.Clone();
+									_displayLine(bestLine);
+								}
+							}
+						}
+					}
+
+					//We've finished this depth's moves, move higher
+					depth++;
+				}
 			}
 
 			// For each searchdepth from forcedMoveCount+1 to targetDepth
@@ -140,20 +207,33 @@ namespace fpli {
 			//		return best evaluation
 		}
 
-		float _winExpectancyHome(int homeId, int awayId) {
+		double _winExpectancyHome(int homeId, int awayId) {
 			Team home = _fpl.Bootstrap.teams.Find(t => t.id == homeId);
 			Team away = _fpl.Bootstrap.teams.Find(t => t.id == awayId);
 
-			float dr = (away.strength_overall_away - home.strength_overall_home);
-			return (float) (1f/(Math.Pow(10f, dr/400f) +1f));
+			double dr = (away.strength_overall_away - home.strength_overall_home);
+			return 1f/(Math.Pow(10f, dr/400f) +1f);
 		}
 
-		float _winExpectancyAway(int homeId, int awayId) {
+		double _winExpectancyAway(int homeId, int awayId) {
 			Team home = _fpl.Bootstrap.teams.Find(t => t.id == homeId);
 			Team away = _fpl.Bootstrap.teams.Find(t => t.id == awayId);
 
-			float dr = (home.strength_overall_home - away.strength_overall_away);
-			return (float) (1f/(Math.Pow(10f, dr/400f) +1f));
+			double dr = (home.strength_overall_home - away.strength_overall_away);
+			return 1f/(Math.Pow(10f, dr/400f) +1f);
+		}
+
+
+		// A line starts at the node and works to the root though we'll need to 
+		// display it in inverse order
+
+		void _displayLine(TeamScoreEval[] line) {
+			Console.Write($"{line[0].eval:0.000} =");
+			for (int i = line.Length -1; i >= 0; --i) {
+				string teamName = _fpl.Bootstrap.teams.Find(t => t.id == line[i].team).short_name;
+				Console.Write($"  {teamName} {line[i].eval:0.000}");
+			}
+			Console.WriteLine("");
 		}
 	}
 
